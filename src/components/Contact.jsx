8627190c917b66c301449e9861efefea5dba5592
Zobayada <azobayada@gmail.com>
2023-03-26 01:12:22 +0600

    contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,11 +22,52 @@ const Contact = () => {
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h2 className={styles.sectionHeadText}>Contact.</h2>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
 
         <form ref={formRef}
           onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"></form>
+          className="mt-12 flex flex-col gap-8">
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name :</span>
+            <input type="text"
+              name="name"
+              onChange={handleChange}
+              value={form.name}
+              placeholder="Enter Your Name"
+              className="bg-tertiary px-6 py-4 placeholder:text-secondary text-white  rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email :</span>
+            <input type="email"
+              name="email"
+              onChange={handleChange}
+              value={form.email}
+              placeholder="Enter Your Email"
+              className="bg-tertiary px-6 py-4 placeholder:text-secondary text-white  rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message :</span>
+            <textarea rows="7"
+              name="message"
+              onChange={handleChange}
+              value={form.message}
+              placeholder="Enter Your Message"
+              className="bg-tertiary px-6 py-4 placeholder:text-secondary text-white  rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <button type="submit"
+            className="bg-tertiary py-3 px-8 w-fit ouline-none font-bold text-white shadow-md shadow-primary rounded-xl">
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+        <EarthCanvas />
       </motion.div>
     </div>
   )
